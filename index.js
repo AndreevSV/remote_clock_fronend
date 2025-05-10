887b435f1b468c8acf5e93e689ddd865d21068d5
AndreevSV <andreevsv@gmail.com>
2025-05-10 21:08:06 +0300

    fix: change server to the web

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// const SERVER =
-// 'https://remoteclockservercsharp20250502005837-a3gjeuc9engqf9ac.canadacentral-01.azurewebsites.net/';
-const SERVER = 'http://localhost:5252';
+const SERVER =
+'https://remoteclockservercsharp20250502005837-a3gjeuc9engqf9ac.canadacentral-01.azurewebsites.net/';
+// const SERVER = 'http://localhost:5252';
 
 let dateTimeDto;
 
